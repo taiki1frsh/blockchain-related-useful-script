d58853cfbfa0f6b9fe74d162b751f0815d1ef2fd
Taiki_F <taiki.furusyo@gmail.com>
2022-07-07 08:41:19 +0900

    fix error

--- start-cosmos.sh
+++ start-cosmos.sh
@@ -8,15 +8,18 @@ rm -fr ~/.$DAEMON;
 
 $DAEMON init --chain-id test test;
 
-$DAEMON keys add val;
-$DAEMON keys add usr;
-$DAEMON keys add usr2;
 
-$DAEMON add-genesis-account $($DAEMON keys show -a val) 1000000000$DENOM, 1000000000$STAKE_DENOM;
+$DAEMON keys mnemonic > val.txt;
+$DAEMON keys mnemonic > usr.txt;
+$DAEMON keys mnemonic > usr2.txt;
+
+$DAEMON keys add --recover val;
+$DAEMON keys add --recover usr;
+$DAEMON keys add --recover usr2;
+
+$DAEMON add-genesis-account $($DAEMON keys show -a val) 1000000000$DENOM,1000000000$STAKE_DENOM;
 $DAEMON add-genesis-account $($DAEMON keys show -a usr) 1000000000$DENOM;
 $DAEMON add-genesis-account $($DAEMON keys show -a usr2) 1000000000$DENOM;
-$DAEMON gentx validator 100000000$STAKE_DENOM --chain-id=test;
+$DAEMON gentx val 100000000$STAKE_DENOM --chain-id=test;
 $DAEMON collect-gentxs;
 $DAEMON start;
-
-
